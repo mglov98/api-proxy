9ee94f6c4a7193e15b6e08f8e8d348de8947bbff
luishu <16166850+luishu@user.noreply.gitee.com>
2025-09-15 15:02:39 +0800

    test2

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/api/python")
+@app.route("/python")
 def hello_world():
     return "<p>Hello, World!</p><br/><p>This is a Python Flask API</p>"
